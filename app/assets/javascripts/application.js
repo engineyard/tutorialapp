6ba7c878f641f3643ab5de75acce2b0b0c790dea
Christopher Rigor <crigor@gmail.com>
2017-08-17 10:46:32 +0800

    Add search text field and Javascript code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,4 +20,9 @@ $(document).ready(function() {
     $(this).parents('li').fadeOut(2000);
     event.preventDefault(); 
   });
+
+  $('[data-js-search]').change(function(event) {
+    search_term = $(this).val();
+    alert('You are searching for ' + search_term);
+  });
 });
